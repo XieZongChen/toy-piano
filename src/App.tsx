ecbfdfb016b44df22c4636c2cd71cebc9d705585
XieZongChen <952937570@qq.com>
2025-01-22 10:27:51 +0800

    feat: 开发布局 & 钢琴样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { styled, createGlobalStyle, css } from 'styled-components';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const keys: Record<string, { frequency: number }> = {
+    A: {
+      frequency: 196, // 音节对应声音频率
+    },
+    S: {
+      frequency: 220,
+    },
+    D: {
+      frequency: 246,
+    },
+    F: {
+      frequency: 261,
+    },
+    G: {
+      frequency: 293,
+    },
+    H: {
+      frequency: 329,
+    },
+    J: {
+      frequency: 349,
+    },
+    K: {
+      frequency: 392,
+    },
+  };
+
+  // 全局样式
+  const GlobalStyles = createGlobalStyle`
+    body {
+      background: #000;
+    }
+  `;
+
+  // styled.xxx 写样式组件
+  const KeysStyle = styled.div`
+    width: 800px;
+    height: 400px;
+    margin: 40px auto;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    overflow: hidden;
+  `;
+  
+  // 创建复用的 css 片段
+  const textStyle = css`
+    line-height: 500px;
+    text-align: center;
+    font-size: 50px;
+  `;
+
+  const KeyStyle = styled.div`
+    border: 4px solid black;
+    background: #fff;
+    flex: 1;
+    ${textStyle}
+
+    &:hover {
+      background: #aaa;
+    }
+  `;
+
+  const play = (key: string) => {
+    const frequency = keys[key]?.frequency;
+    if (!frequency) {
+      return;
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <KeysStyle as='section'>
+      {Object.keys(keys).map((item: any) => {
+        return (
+          <KeyStyle as='div' key={item}>
+            <div onClick={() => play(item)}>
+              <span>{item}</span>
+            </div>
+          </KeyStyle>
+        );
+      })}
+      <GlobalStyles />
+    </KeysStyle>
+  );
 }
 
-export default App
+export default App;
